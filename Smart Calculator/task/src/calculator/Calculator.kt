c43df4f693895b8275517e58fe4b0222c38452f9
Egor <ustas1234@gmail.com>
2020-09-04 08:35:37 +0300

    got checking and creating list of operation

--- Smart Calculator/task/src/calculator/Calculator.kt
+++ Smart Calculator/task/src/calculator/Calculator.kt
@@ -1,76 +1,107 @@
 package calculator
 
+import java.math.BigInteger
+
 class Calculator {
     companion object {
         private lateinit var uInput: String
-        private lateinit var convertedInput: MutableList<String>
+        private var convertedInput = MutableList(0) { "" }
+        private var listOfOperations = mutableMapOf<Int, String>()
+        private lateinit var finalResult: BigInteger
         private var isInputError = false
 
         fun start(str: String) {
+            convertedInput.clear()
+            listOfOperations.clear()
+            finalResult = 0.toBigInteger()
             isInputError = false
             uInput = str
+
             checkForGeneralErrors()
             if (!isInputError) {
-                dropUnnecessarySymbols()
+                tryToCalculate()
+                println(finalResult)
+            } else println("Invalid expression")
+
+        }
+
+        private fun tryToCalculate() {
+
+            // println("первый элемент" + convertedInput[0].toBigInteger())
+            // println("размер convertedinput" + convertedInput.size)
+            if (convertedInput.size == 1) finalResult = convertedInput[0].toBigInteger() else {
+                //try to find multiplication, dividing and power
+                for (i in 1..convertedInput.lastIndex step 2) {
+                    if (convertedInput[i].contains(("[*^/]").toRegex())) {
+                        listOfOperations[i] = convertedInput[i]
+                    }
+                }
+                for (i in 1..convertedInput.lastIndex step 2) {
+                    if (convertedInput[i].contains(("[-+]").toRegex())) {
+                        listOfOperations[i] = convertedInput[i]
+                    }
+                }
+                println(listOfOperations)
             }
         }
 
-        private fun dropUnnecessarySymbols() {
+        private fun dropUnnecessarySymbols(i: Int) {
             var tempList = uInput.split(' ') as MutableList
+            var res = tempList[i]
 
-            for (i in 0..tempList.lastIndex) {
+            //println("Before removal: $res")
+            while (res.lastIndex > 0) {
 
-                if (i % 2 == 1 && tempList[i].contains(("[+-]").toRegex()) &&
-                        tempList[i].contains(("[^-+]").toRegex())) {
-                    var res = tempList[i]
-
-                    println("Before removal: $res")
-
-                    while (res.lastIndex > 0) {
-
-                        when (res[0]) {
-                            '+' -> {
-                                res = res.drop(1)
-                            }
-                            '-' -> {
-                                when (res[1]) {
-                                    '+' -> res = '-' + res.drop(2)
-                                    '-' -> res = '+' + res.drop(2)
-                                }
-                            }
-
+                when (res[0]) {
+                    '+' -> {
+                        res = res.drop(1)
+                    }
+                    '-' -> {
+                        when (res[1]) {
+                            '+' -> res = '-' + res.drop(2)
+                            '-' -> res = '+' + res.drop(2)
                         }
-
                     }
 
-                    println("After removal: $res")
-                    tempList[i] = res
                 }
+
             }
+
+            //println("After removal: $res")
+            convertedInput[i] = res
+            // println(convertedInput)
+
+
         }
 
         private fun checkForGeneralErrors() {
             var tempList: MutableList<String> =
                     uInput.split(' ') as MutableList<String>
+            convertedInput = MutableList(tempList.size) { "" }
             for (i in tempList.indices) {
+                convertedInput[i] = tempList[i]
                 if (i % 2 == 0) {
                     if (tempList[i].toBigIntegerOrNull() == null) invalidExpressionAlert()
                 } else {
                     //проверяем символы в нечетных элементах
-                     println(tempList[i])
-                    if (tempList[i].contains(("[^-*^+()]").toRegex())) invalidExpressionAlert()
+                    //    println(tempList[i])
+                    if (tempList[i].contains(("[^-*^+()/]").toRegex())) invalidExpressionAlert()
                     //проверяем последовательность + и -
-                    if (tempList[i].contains(("[+-]").toRegex()) &&
-                            tempList[i].contains(("[^-+]").toRegex())) invalidExpressionAlert()
+                    if (tempList[i].contains(("[+-]").toRegex())) {
+                        if (tempList[i].contains(("[^-+]").toRegex())) invalidExpressionAlert() else {
+                            dropUnnecessarySymbols(i)
+                        }
+                    }
+
 
                 }
             }
-             println(tempList)
+            // println(tempList)
 
         }
 
         private fun invalidExpressionAlert() {
-            println("Invalid expression")
+            //println("Invalid expression")
             isInputError = true
         }
 
